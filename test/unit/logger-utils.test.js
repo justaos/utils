65bc4ecb4c1ffa6bf1fc6ad17d1d4f2fe85f5c2d
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2019-12-02 18:41:45 +0530

    test flapper fix

--- test/unit/logger-utils.test.js
+++ test/unit/logger-utils.test.js
@@ -10,6 +10,9 @@ describe('logger-utils', function () {
 
     let logger;
 
+    before(function(){
+        clearLogFile();
+    });
 
 
     it('#createLogger()', function () {
@@ -17,25 +20,34 @@ describe('logger-utils', function () {
         assert.isOk(!!logger, "Logger created");
     });
 
-    it('#info()', function () {
+    it('#info()', function (done) {
         logger.info("test");
-        const fileContent = utils.readFileSync(LOG_FILEPATH);
-        const logStr = fileContent.toString();
-        expect(logStr).to.contain("info: test");
+        setTimeout(function() {
+            const fileContent = utils.readFileSync(LOG_FILEPATH);
+            const logStr = fileContent.toString();
+            expect(logStr).to.contain("info: test");
+            done();
+        }, 500);
+
     });
 
-    it('#error()', function () {
+    it('#error()', function (done) {
         logger.error("test");
-        const fileContent = utils.readFileSync(LOG_FILEPATH);
-        const logStr = fileContent.toString();
-        expect(logStr).to.contain("error: test");
+        setTimeout(function() {
+            const fileContent = utils.readFileSync(LOG_FILEPATH);
+            const logStr = fileContent.toString();
+            expect(logStr).to.contain("error: test");
+            done();
+        }, 500);
     });
 
-    it('#warn()', function () {
+    it('#warn()', function (done) {
         logger.warn("test");
-        const fileContent = utils.readFileSync(LOG_FILEPATH);
-        const logStr = fileContent.toString();
-        expect(logStr).to.contain("warn: test");
-        clearLogFile();
+        setTimeout(function() {
+            const fileContent = utils.readFileSync(LOG_FILEPATH);
+            const logStr = fileContent.toString();
+            expect(logStr).to.contain("warn: test");
+            done();
+        }, 500);
     });
 });
