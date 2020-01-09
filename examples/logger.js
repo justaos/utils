b7f2b5b85bd0188865146ae1f41e3859c15f5da8
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2020-01-09 23:18:19 +0530

    minor change to test github actions

--- examples/logger.js
+++ examples/logger.js
@@ -2,5 +2,5 @@ const {createLogger} = require('../lib/logger-utils');
 
 const logger = createLogger({filePath: __dirname + '/log/test.log', label: 'logger.js'});
 logger.info("some info message");
-logger.error("Some error");
+logger.error("some error");
 logger.logError(new Error("this is a error object"));
