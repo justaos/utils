50e596b7d5e14b3c6ba63105f99c17b8477647d2
Shivaji Varma <contact@shivajivarma.com>
2020-08-05 20:53:04 +0530

    email receiver implementation

--- src/email-utils.ts
+++ src/email-utils.ts
@@ -2,9 +2,10 @@ import * as nodemailer from "nodemailer";
 import * as Mail from "nodemailer/lib/mailer";
 import * as Imap from 'imap';
 import {createLogger} from "./logger-utils";
-import {simpleParser} from 'mailparser';
+const parse = require('parse-email');
 // @ts-ignore
 import * as replyParser from "node-email-reply-parser";
+
 
 export class EmailSendHandler {
 
@@ -29,27 +30,34 @@ export class EmailReceiveHandler {
 
     constructor(config: any, callback: any) {
         let firstSkipped = false;
+        let total = 1;
 
         this.#imap = new Imap(config);
 
         this.#imap.once('ready', () => {
             logger.info("IMAP receiver ready");
             this.#imap.openBox('INBOX', true, (err, box) => {
-
+                total = box.messages.total;
+                console.log("total :: " + total);
             });
         });
 
         this.#imap.on('mail', (numNewMsgs: number) => {
-            const fetch = this.#imap.seq.fetch(numNewMsgs + ":*", {
+            if (!firstSkipped) {
+                firstSkipped = true
+                return;
+            }
+
+            const fetch = this.#imap.seq.fetch(total + ":*", {
                 bodies: ['']
             });
 
+            total = total + numNewMsgs;
+
 
             fetch.on('message', (msg, seqno) => {
-                if (!firstSkipped) {
-                    firstSkipped = true
-                    return;
-                }
+
+                console.log("#seqno :: " + seqno);
                 msg.on('body', function (stream, info) {
 
                     let buffer = '';
@@ -57,9 +65,9 @@ export class EmailReceiveHandler {
                         buffer += chunk.toString('utf8');
                     });
                     stream.once('end', function () {
-                        simpleParser(stream).then(function (parsedMail) {
+                        parse(buffer).then(function (parsedMail: any) {
                             callback(buffer, parsedMail);
-                        }).catch(function (err) {
+                        }).catch(function (err: Error) {
                             callback(err);
                             console.log('An error occurred:', err.message);
                         });
