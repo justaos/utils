6157e2b3f11d83a7a8d7257dfb7dfe38a1cd696e
Shivaji Varma <contact@shivajivarma.com>
2020-10-17 15:17:25 +0530

    added string functions

--- src/string-utils.ts
+++ src/string-utils.ts
@@ -5,3 +5,23 @@ export function underscoreToCamelCase(input: string): string {
   });
   return input.replace('_', ' ');
 }
+
+export function hasDuplicates(a: string[]): boolean {
+  for (let i = 0; i <= a.length; i++)
+    for (let j = i; j <= a.length; j++) if (i != j && a[i] == a[j]) return true;
+  return false;
+}
+
+export const findDuplicates = (arr: string[]) => {
+  const sortedArray = arr.slice().sort(); // You can define the comparing function here.
+  // JS by default uses a crappy string compare.
+  // (we use slice to clone the array so the
+  // original array won't be modified)
+  const results = [];
+  for (let i = 0; i < sortedArray.length - 1; i++) {
+    if (sortedArray[i + 1] == sortedArray[i]) {
+      results.push(sortedArray[i]);
+    }
+  }
+  return results;
+};
