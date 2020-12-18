b198142b2eeda5bb7128e6177546a281052b0d7e
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2020-12-18 16:27:18 +0530

    delete operation added

--- packages/file-utils/src/index.ts
+++ packages/file-utils/src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs-extra';
 import * as glob from 'glob';
 import * as jsonfile from 'jsonfile';
+import del from 'del';
 
 export default class FileUtils {
   static readJsonFileSync(filePath: string, options?: any): any {
@@ -26,6 +27,10 @@ export default class FileUtils {
 
   static readFileSync(filePath: string, options?: any): Buffer {
     return fs.readFileSync(filePath, options);
+  }
+
+  static delete(patterns: any) {
+    return del(patterns);
   }
 
   static copySync(sourcePath: string, destinationPath: string) {
