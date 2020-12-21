9825e8ac27daf245dfce54fe81d9b1debf119c96
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2020-12-21 15:09:20 +0530

    flatToHierarchy added

--- packages/ds-utils/src/index.ts
+++ packages/ds-utils/src/index.ts
@@ -31,4 +31,28 @@ export default class DSUtils {
     }
     return results;
   }
+
+  static flatToHierarchy(flat: any[]): any[] {
+    const roots: any[] = []; // things without parent
+    const all: any = {};
+
+    flat.forEach((item: any) => {
+      all[item.id] = item;
+    });
+
+    // connect children to its parent, and split roots apart
+    for (const key of Object.keys(all)) {
+      const item = all[key];
+      if (!item.parent) {
+        roots.push(item);
+      } else if (item.parent in all) {
+        const p = all[item.parent];
+        if (!('children' in p)) p.children = [];
+        p.children.push(item);
+      }
+    }
+
+    // done!
+    return roots;
+  }
 }
