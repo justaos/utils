1d65a86b1e5b23356281b4aba4d78fee66636387
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2020-12-21 15:35:14 +0530

    test added

--- packages/ds-utils/test/unit/ds-utils.test.ts
+++ packages/ds-utils/test/unit/ds-utils.test.ts
@@ -1,11 +1,17 @@
 import { assert } from 'chai';
 import DSUtils from '../../src';
 
-describe('ds-utils', function() {
-
-  it('#underscoreToCamelCase()', function() {
+describe('ds-utils', function () {
+  it('#underscoreToCamelCase()', function () {
     assert.equal(DSUtils.underscoreToCamelCase('hello_world'), 'Hello World');
-    assert.equal(DSUtils.underscoreToCamelCase('_hello_world_'), ' Hello World ');
+    assert.equal(
+      DSUtils.underscoreToCamelCase('_hello_world_'),
+      ' Hello World '
+    );
   });
 
+  it('#hasDuplicates()', function () {
+    assert.equal(DSUtils.hasDuplicates(['a', 'a']), true);
+    assert.equal(DSUtils.hasDuplicates(['a', 'ab']), false);
+  });
 });
