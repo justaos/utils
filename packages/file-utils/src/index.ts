542b8a9a38fe23298cd2da1174bd96d196cd3cca
Shivaji Varma <contact@shivajivarma.com>
2022-02-13 12:19:09 +0530

    WIP: remove email-utils

--- packages/file-utils/src/index.ts
+++ packages/file-utils/src/index.ts
@@ -21,12 +21,32 @@ export default class FileUtils {
     jsonfile.writeFileSync(filePath, object, { spaces: 2 });
   }
 
-  static writeFileSync(filePath: string, content: string) {
-    fs.writeFileSync(filePath, content);
+  static writeFileSync(filePath: string, content: string, options?: any) {
+    fs.writeFileSync(filePath, content, options);
+  }
+
+  static async writeFile(
+    filePath: string,
+    content: string,
+    options?: any
+  ): Promise<void> {
+    return fs.writeFile(filePath, content, options);
   }
 
   static readFileSync(filePath: string, options?: any): Buffer {
     return fs.readFileSync(filePath, options);
+  }
+
+  static async readFile(filePath: string, options?: any): Promise<any> {
+    new Promise((resolve, reject) => {
+      fs.readFile(filePath, options, (err: any, data: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
   }
 
   static delete(patterns: any): Promise<any> {
